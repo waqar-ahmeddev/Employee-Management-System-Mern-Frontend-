2959fdc6ed4c1dfc52018b9306b1edd6251caa80
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-06-06 20:06:15 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <button className="bg-green-500 text-white px-6 py-3 rounded-lg hover:bg-green-600 transition cursor-pointer">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
